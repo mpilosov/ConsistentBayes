bb5146b57b6a8065404344a91eb6a836339abab2
M. Pilosov <michael.pilosov@ucdenver.edu>
2018-01-20 12:35:10 -0700

    distribution test ammended.

--- tests/distributions_test.py
+++ tests/distributions_test.py
@@ -1,17 +1,22 @@
 from nose import with_setup # optional
 from cbayes import supported_distributions
 from scipy.stats import _continuous_distns
+from nose.tools import assert_equals
+
+def test_supp_dist():
+    print('========== testing supported_distributions for no arguments ==========')
+    assert_equals(type(supported_distributions()), dict)
 
 def test_supp_dist_norm():
     print('========== testing supported_distributions for Normal ==========')
-    assert type(supported_distributions('Normal')) is _continuous_distns.norm_gen
-    assert type(supported_distributions('Gauss')) is _continuous_distns.norm_gen
-    assert type(supported_distributions('Gaussian')) is _continuous_distns.norm_gen
-    assert type(supported_distributions('Norm')) is _continuous_distns.norm_gen
-    assert type(supported_distributions('N')) is _continuous_distns.norm_gen
+    assert_equals(type(supported_distributions('Normal')), _continuous_distns.norm_gen)
+    assert_equals(type(supported_distributions('Gauss')), _continuous_distns.norm_gen)
+    assert_equals(type(supported_distributions('Gaussian')), _continuous_distns.norm_gen)
+    assert_equals(type(supported_distributions('Norm')), _continuous_distns.norm_gen)
+    assert_equals(type(supported_distributions('N')), _continuous_distns.norm_gen)
 
 def test_supp_dist_uni():
     print('========== testing supported_distributions for Uniform ==========')
-    assert type(supported_distributions('Uniform')) is _continuous_distns.uniform_gen
-    assert type(supported_distributions('U')) is _continuous_distns.uniform_gen
-    assert type(supported_distributions('Uni')) is _continuous_distns.uniform_gen
+    assert_equals(type(supported_distributions('Uniform')), _continuous_distns.uniform_gen)
+    assert_equals(type(supported_distributions('U')), _continuous_distns.uniform_gen)
+    assert_equals(type(supported_distributions('Uni')), _continuous_distns.uniform_gen)
