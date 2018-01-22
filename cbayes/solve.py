138fc70bac08fe0dd8902a3f6f49d52d6e954f92
Michael Pilosov <michael.pilosov@ucdenver.edu>
2018-01-21 18:31:28 -0700

    added solve method.

--- cbayes/solve.py
+++ cbayes/solve.py
@@ -3,10 +3,8 @@
 
 r"""
 This module contains the methods for solving the stochastic inverse problem:
-    :method:`cbayes.solve.compute_ratio`
-    :method:`bet.sample.discretization`
-    :class:`bet.sample.length_not_matching`
-    :class:`bet.sample.dim_not_matching`
+    :method:`cbayes.solve.perform_accept_reject`
+
 """
 
 import cbayes.sample as samp
@@ -14,42 +12,52 @@ import cbayes.sample as samp
 #: TODO add logging/warnings, save options, load options.
 # import os, logging 
 
-def compute_ratio(problem_set):
+def perform_accept_reject(samples, ratios, seed=0):
     r"""
-    TODO: Add description
-    :param sample_set: 
-    :type sample_set: :class:`~/cbayes.sample.sample_set`
-    
-    :rtype: :class:`numpy.ndarray` of shape(num,)
-    :returns: ratio of observed to pushforward density evaluations
-    """
-    data = problem_set.output.samples
-    ratio = problem_set.observed_dist.pdf(data) / problem_set.pushforward_dist.pdf(data)
-    ratio = ratio.ravel()
-    problem_set.ratio = ratio
-    return ratio
-
-def perform_accept_reject(problem_set, seed=None):
-    r"""
+    TODO: CHECK SIZES!!! samples and ratios should match up.
     Perform a standard accept/reject procedure by comparing 
     normalized density values to draws from Uniform[0,1]
     
-    :param sample_set: 
-    :type sample_set: :class:`~/cbayes.sample.sample_set`
+    :param samples: Your samples.
+    :type samples: :class:`~/cbayes.sample.sample_set` of shape (num, dim)
     
+    :param ratios:
+    :type ratios: :class:`numpy.ndarray` of shape (num,)
     :param int seed: Your seed for the accept/reject.
     
     It is encouraged that you run this multiple times when num_samples is small
     Then, average the results to get an average acceptance rate.
     """
-    M = np.max(problem_set.ratio)
-    eta_r = problem_set.ratio/M
-    if seed is None:
-        np.random.seed(problem_set.seed)
-    else:
-        np.random.seed(seed)
-    problem_set.accept_inds = [i for i in range(problem_set.input.num_samples) 
+    num_samples = len(ratios)
+    #: check dimensions here.
+    
+    #: normalize the ratios to [0, 1]
+    M = np.max(ratios)
+    eta_r = ratios/M
+    
+    np.random.seed(seed)
+    accept_inds = [i for i in range(num_samples) 
                 if eta_r[i] > np.random.rand() ] 
-    pass
+    return accept_inds
 
 #: TODO ADD A LOT MORE METHODS. Weighted KDE, surrogate post, MCMC, etc.
+
+def solve(problem_set, method='AR', seed=0):
+    r"""
+    This solves the inverse problem. It's a wrapper for other functions.
+    
+    :param problem_set: Your problem_set.
+    :type problem_set: :class:`~/cbayes.sample.problem_set` 
+
+    :param str method: One of the supported methods ('AR' for accept/reject)
+    """
+    samples = problem_set.input.samples 
+    ratios = problem_set.ratio
+    
+    if method == 'AR':
+        accept_inds = perform_accept_reject(samples, ratios, seed)
+    else:
+        raise TypeError("method given not supported. Please see documentation.")
+    
+    problem_set.accept_inds = accept_inds
+    pass
