10e78505855d6906bd245a4efadaca4fd33c9865
Michael Pilosov <michael.pilosov@ucdenver.edu>
2018-01-25 14:25:10 -0700

    widget dependency handled.

--- fresh_install.sh
+++ fresh_install.sh
@@ -6,5 +6,7 @@ conda create -n test_py_env python=3.6 -y &&
 source activate test_py_env && python setup.py install && nosetests
 
 python -m ipykernel install --name test_py_env --user
+jupyter nbextension install --py --user widgetsnbextension
+jupyter nbextension enable --py widgetsnbextension
 jupyter-notebook
 
