1d98196589c8640ddcaa0dcf4ef7d404dbbee9bc
Michael Pilosov <michael.pilosov@ucdenver.edu>
2018-02-06 16:42:07 -0700

    added ipykernel to fresh install

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,6 +5,7 @@ conda remove -n test_py_env --all -y
 conda create -n test_py_env python=3.6 -y && 
 source activate test_py_env && python setup.py install && nosetests
 
+pip install ipykernel
 python -m ipykernel install --name test_py_env --user
 jupyter nbextension install --py --user widgetsnbextension
 jupyter nbextension enable --py widgetsnbextension
