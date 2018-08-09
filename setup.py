cbc46a0e4124f80338daf0fd2cce01413fd9966e
M. Pilosov <michael.pilosov@ucdenver.edu>
2018-08-09 15:29:18 -0600

    standard updates.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='cbayes',
-      version='0.3.30',
+      version='0.3.31',
       description='Consistent Bayesian Inversion',
       author='Michael Pilosov',
       author_email='mpilosov@gmail.com',
